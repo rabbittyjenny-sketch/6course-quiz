02b44c7f59bb30b5aa72130ffbbc3f5d998b8c3d
JYNE <rabbitty.jenny@gmail.com>
2026-05-14 14:10:02 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import LMSv2Tool from './Creatr365_LMS_v2';
+
+function App() {
+  return (
+    <div className="App">
+      <LMSv2Tool />
+    </div>
+  );
+}
+
+export default App;
